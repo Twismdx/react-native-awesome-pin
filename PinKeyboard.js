c6fbd068aa1887af55d27ccd240f4dbdefeffd40
Damien <80352555+Twismdx@users.noreply.github.com>
2025-05-10 17:57:27 +0930

    Update PinKeyboard.js

--- PinKeyboard.js
+++ PinKeyboard.js
@@ -1,232 +1,142 @@
-/**
- * @author Luke Brandon Farrell
- * @description Keyboard component with error status bar and interactive keys
- */
+import React, { Component } from "react";
+import {
+  View,
+  Image,
+  Text,
+  StyleSheet,
+  Platform,
+  Pressable
+} from "react-native";
+import PropTypes from "prop-types";
 
-import React, { Component } from "react"
-import { View, Image, Text, StyleSheet, Platform, TouchableOpacity } from "react-native"
-import { Pressable } from "react-native"
-import PropTypes from "prop-types"
-
-const backAsset = require("./back.png")
+const backAsset = require("./back.png");
 
 class PinKeyboard extends Component {
-  /**
-   * [ Built-in React method. ]
-   *
-   * Setup the component. Executes when the component is created
-   *
-   * @param {object} props
-   *
-   */
   constructor(props) {
-    super(props)
-
+    super(props);
     this.state = {
       disabled: false,
       error: null
-    }
+    };
   }
 
-  /**
-   * [ Built-in React method. ]
-   *
-   * Executed when the component is mounted to the screen.
-   */
   componentDidMount() {
-    this.props.onRef(this)
+    this.props.onRef(this);
   }
 
-  /**
-   * [ Built-in React method. ]
-   *
-   * Executed when the component is unmounted from the screen
-   */
   componentWillUnmount() {
-    this.props.onRef(undefined)
+    this.props.onRef(undefined);
   }
 
-  /**
-   * [ Built-in React method. ]
-   *
-   * Allows us to render JSX to the screen
-   */
   render() {
-    /** Styles */
-    const { containerStyle, keyboardDefaultStyle, keyboardRowStyle } = styles
-    /** Props */
-    const {
-      keyboard,
-      // Style Props
-      keyboardStyle
-    } = this.props
-
+    const { containerStyle, keyboardDefaultStyle, keyboardRowStyle } = styles;
+    const { keyboard, keyboardStyle, keyboardDisabledStyle } = this.props;
     return (
       <View style={containerStyle}>
         {this.renderError()}
-        <View style={[keyboardDefaultStyle, keyboardStyle]}>
-          {// Maps each array of numbers in the keyboardValues array
-            keyboard.map((row, r) => {
-              return (
-                <View key={r} style={keyboardRowStyle}>
-                  {// Maps each number in row and creates key for that number
-                    row.map((element, k) => {
-                      return this.renderKey(element, r, k)
-                    })}
-                </View>
-              )
-            })}
+        <View style={[keyboardDefaultStyle, keyboardStyle, keyboardDisabledStyle]}>
+          {keyboard.map((row, r) => (
+            <View key={r} style={keyboardRowStyle}>
+              {row.map((element, k) => this.renderKey(element, r, k))}
+            </View>
+          ))}
         </View>
       </View>
-    )
+    );
   }
 
-  /**
-   * Renders the error
-   *
-   * @return {*}
-   */
   renderError() {
-    // Styles
-    const { errorDefaultStyle, errorTextDefaultStyle } = styles
-
-    // Props
-    const {
-      // Style Props
-      errorStyle,
-      errorTextStyle
-    } = this.props
-
-    // State
-    const { error } = this.state
-
-    if (error) {
-      return (
-        <View style={[errorDefaultStyle, errorStyle]}>
-          <Text style={[errorTextDefaultStyle, errorTextStyle]}>{error}</Text>
-        </View>
-      )
-    }
-
-    return null
+    const { errorDefaultStyle, errorTextDefaultStyle } = styles;
+    const { errorStyle, errorTextStyle } = this.props;
+    const { error } = this.state;
+    if (!error) return null;
+    return (
+      <View style={[errorDefaultStyle, errorStyle]}>
+        <Text style={[errorTextDefaultStyle, errorTextStyle]}>{error}</Text>
+      </View>
+    );
   }
 
-  /**
-   * Renders a key on the keyboard
-   *
-   * @param entity
-   * @param row
-   * @param column
-   *
-   * @return {jsx}
-   */
   renderKey(entity, row, column) {
-    if (typeof this.props.renderKey === "function") {
-      return this.props.renderKey(entity, row, column)
-    }
-    /** Styles */
-    const {
-      keyContainerStyle,
-      keyboardDisabledDefaultStyle,
-      keyDefaultStyle,
-      keyTextDefaultStyle,
-      keyImageDefaultStyle
-    } = styles
-    /** Props */
     const {
       disableRippleEffect,
       keyDown,
       keyboardFunc,
-      keyboardDisabledStyle,
-      // Style Props
       keyStyle,
       keyTextStyle,
       keyImageStyle
-    } = this.props
-    /** State */
-    const { disabled } = this.state
+    } = this.props;
 
-    // Custom functions for the keyboard key
-    const keyboardFuncSet = keyboardFunc
-      ? keyboardFunc
-      : [
+    const {
+      keyContainerStyle: defaultContainerStyle,
+      keyDefaultStyle: defaultKeyStyle,
+      keyTextDefaultStyle: defaultTextStyle,
+      keyImageDefaultStyle: defaultImageDefaultStyle
+    } = styles;
+
+    // build the function map (falling back to built-in back)
+    const keyboardFuncSet =
+      keyboardFunc || [
         [null, null, null],
         [null, null, null],
         [null, null, null],
-        [null, 0, () => this.props.keyDown("back")]
-      ]
+        [null, null, () => keyDown("back")]
+      ];
 
-    // Decide if the element passed as the key is text
-    const keyJsx = keyboardFuncSet[row][column] ? (
-      <Image style={[keyImageDefaultStyle, keyImageStyle]} source={entity} />
-    ) : (
-      <Text style={[keyTextDefaultStyle, keyTextStyle]}>{entity}</Text>
-    )
+    const fn = keyboardFuncSet[row][column];
+    const onPress = () => (fn ? fn() : keyDown(entity));
 
-    // We want to block keyboard interactions if it has been disabled.
-    if (!disabled) {
-      // Always use Pressable; use android_ripple when ripple is enabled
-      const onPress = () =>
-        keyboardFuncSet[row][column]
-          ? keyboardFuncSet[row][column]()
-          : keyDown(entity)
-
-      return (
-        <Pressable
-          key={column}
-          onPress={onPress}
-          android_ripple={!disableRippleEffect ? { color: "#000" } : undefined}
-          style={
-            ({ pressed }) => [
-              keyContainerStyle,
-              keyDefaultStyle,
-              keyStyle,
-              pressed && !disableRippleEffect
-                ? { opacity: 0.6 }
-                : null,
-            ]
-          }
-        >
-          {keyJsx}
-        </Pressable >
-      )
+    // decide what goes inside the button
+    let content;
+    if (typeof this.props.renderKey === "function") {
+      content = this.props.renderKey(entity, row, column);
+    } else if (fn) {
+      content = (
+        <Image
+          style={[defaultImageDefaultStyle, keyImageStyle]}
+          source={entity}
+        />
+      );
+    } else {
+      content = (
+        <Text style={[defaultTextStyle, keyTextStyle]}>{entity}</Text>
+      );
     }
+
+    return (
+      <Pressable
+        key={column}
+        onPress={onPress}
+        android_ripple={
+          !disableRippleEffect ? { color: "#000" } : undefined
+        }
+        style={({ pressed }) => [
+          defaultContainerStyle,
+          defaultKeyStyle,
+          keyStyle,
+          pressed && !disableRippleEffect ? { opacity: 0.6 } : null
+        ]}
+        disabled={this.state.disabled}
+      >
+        {content}
+      </Pressable>
+    );
   }
 
-  /**
-   * Function used to display an error above the keyboard
-   *
-   * @param error
-   */
   throwError(error) {
-    this.setState({
-      error
-    })
+    this.setState({ error });
   }
 
-  /**
-   * Function used to clear the error on the keyboard
-   */
   clearError() {
-    this.setState({ error: null })
+    this.setState({ error: null });
   }
 
-  /**
-   * Function used to disable the keyboard
-   */
   disable() {
-    this.setState({
-      disabled: true
-    })
+    this.setState({ disabled: true });
   }
 
-  /**
-   * Function used to enable the keyboard
-   */
   enable() {
-    this.setState({
-      disabled: false
-    })
+    this.setState({ disabled: false });
   }
 }
 
@@ -235,7 +145,6 @@ PinKeyboard.propTypes = {
   keyDown: PropTypes.func.isRequired,
   keyboard: PropTypes.array,
   keyboardFunc: PropTypes.array,
-  // Style props
   keyboardStyle: PropTypes.object,
   keyboardDisabledStyle: PropTypes.object,
   keyStyle: PropTypes.object,
@@ -244,18 +153,14 @@ PinKeyboard.propTypes = {
   errorStyle: PropTypes.object,
   errorTextStyle: PropTypes.object,
   disableRippleEffect: PropTypes.bool
-}
+};
 
 PinKeyboard.defaultProps = {
-  // Keyboard configuration. The default contains a key
-  // for each number 0 - 9 and a back button.
   keyboard: [[1, 2, 3], [4, 5, 6], [7, 8, 9], [null, 0, backAsset]],
-  // Keyboard functions. By default the text (number) in the
-  // keyboard array will be passed via the keyDown callback.
-  // Use this array to set custom functions for certain keys.
   keyboardFunc: null,
-  keyboardErrorDisplayTime: 3000
-}
+  keyVibration: true,
+  shakeVibration: true
+};
 
 const styles = StyleSheet.create({
   containerStyle: {
@@ -264,8 +169,6 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     justifyContent: "flex-end"
   },
-  // Style applied to the keyboard. Must contain a height or
-  // the keyboard will not be displayed.
   keyboardDefaultStyle: {
     height: 250,
     backgroundColor: "#FFF"
@@ -276,15 +179,9 @@ const styles = StyleSheet.create({
   },
   keyContainerStyle: {
     flex: 1,
-    flexDirection: "column",
     justifyContent: "center",
     alignItems: "center"
   },
-  // Style applied to keyboard when it is disabled.
-  keyboardDisabledDefaultStyle: {
-    backgroundColor: "#FFF"
-  },
-  // Style the individual keys
   keyDefaultStyle: {
     backgroundColor: "#FFF",
     borderRightColor: "#e8e8e8",
@@ -292,22 +189,16 @@ const styles = StyleSheet.create({
     borderBottomColor: "#e8e8e8",
     borderBottomWidth: 1
   },
-  // Style for the text inside a key
   keyTextDefaultStyle: {
     ...Platform.select({
-      ios: {
-        fontFamily: "HelveticaNeue"
-      },
-      android: {
-        fontFamily: "Roboto"
-      }
+      ios: { fontFamily: "HelveticaNeue" },
+      android: { fontFamily: "Roboto" }
     }),
     fontWeight: "400",
     fontSize: 25,
     textAlign: "center",
     color: "#222222"
   },
-  // Style for an image inside a key
   keyImageDefaultStyle: {
     width: 28,
     height: 28
@@ -324,6 +215,6 @@ const styles = StyleSheet.create({
     fontSize: 15,
     fontWeight: "bold"
   }
-})
+});
 
-export default PinKeyboard
+export default PinKeyboard;
